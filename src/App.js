d3d2fc104868504230105f5aa5e373650fc95554
Mark Watney <kietdo0602@gmail.com>
2023-05-06 03:09:17 -0400

    Fix font issues, update logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import Captcha from './Captcha';
-import React, {useState, useEffect, useRef} from 'react';
-import {imageData} from './assets/Data'
-import So from './so.txt';
-// import fs from 'fs';
-import { Buffer } from 'buffer';
-import axios from 'axios';
+import React from 'react';
+
 
 function App() {
   return (
@@ -12,6 +8,7 @@ function App() {
       <h1 className="kaptcha-title">Kaptcha</h1>
       <h2 className="kaptcha-author">Made by <a href="https://github.com/kietdo0602">Kiet</a></h2>
       <p className="kaptcha-desc">Different take on traditional Captchas, utilizing new techniques.</p>
+      <p className="kaptcha-desc">React, Node.js, Express, Mongoose, Heroku</p>
       <b><p className="kaptcha-desc">Click on the circle to start authenticating!</p></b>
       {/* no_questions: 1-5 */}
       {/* question_types: int[] arr, 1 <= len <= 5 */}
